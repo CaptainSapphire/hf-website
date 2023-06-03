b4bb6423c8c4788f79bbb5b66404911ae1fc916d
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2023-06-02 18:59:10 -0700

    1 file modified

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 console.log('Hello!');
 let slideIndex = 0;
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 30000); // Change image every 30 seconds
+}  
 
 if (window.location.pathname == "/index.html") {
   showSlides();
-  function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 30000); // Change image every 30 seconds
-  }  
 }
